969e175b006b61f4d37d08345a78b74a8914c76b
nilsongpk <nilsongpk@gmail.com>
2025-04-28 21:33:56 -0300

    pronto

--- xadrez.c
+++ xadrez.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-
 int main() {
+    // Movimento da Torre
+    printf("Movimento da Torre:\n");
+    int casasTorre = 5; // Número de casas para mover
+    for (int i = 1; i <= casasTorre; i++) {
+        printf("Direita\n");
+    }
+
+    // Movimento do Bispo
+    printf("\nMovimento do Bispo:\n");
+    int casasBispo = 5; // Número de casas para mover
+    int contadorBispo = 1;
+    while (contadorBispo <= casasBispo) {
+        printf("Cima, Direita\n");
+        contadorBispo++;
+    }
+
+    // Movimento da Rainha
+    printf("\nMovimento da Rainha:\n");
+    int casasRainha = 8; // Número de casas para mover
+    int contadorRainha = 1;
+    do {
+        printf("Esquerda\n");
+        contadorRainha++;
+    } while (contadorRainha <= casasRainha);
 
     return 0;
 }
